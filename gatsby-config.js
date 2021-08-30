c6579692074219e1f71c447cfd23b49e04bc9a5f
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2021-08-30 18:13:09 +0100

    Meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,22 @@ module.exports = {
   siteMetadata: {
     title: "form",
   },
+  
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+          name: `Do I need my Sunglasses?`,
+          short_name: `Sun`,
+          start_url: `/`,
+          background_color: `#bada55`,
+          theme_color: `#bada55`,
+          display: `minimal-ui`,
+          icon: `src/images/glasses.png`, // This path is relative to the root of the site.
+      },
+  },
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
